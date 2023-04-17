e5092aa75d3e6f7b5bc625335012406bbfe40c50
deafnv <rokiniri@yahoo.in>
2023-04-17 18:25:26 +0800

    feat: select files, no functionality

--- lib/screens/files.dart
+++ lib/screens/files.dart
@@ -6,6 +6,7 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_downloader/flutter_downloader.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
+import 'package:back_button_interceptor/back_button_interceptor.dart';
 import 'package:path_provider/path_provider.dart';
 import 'package:permission_handler/permission_handler.dart';
 
@@ -24,12 +25,35 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   final apiUrl = dotenv.env['API_URL']!;
+  ApiListResponseList? _data;
+  bool connectionDone = false;
+  bool selectMode = false;
+  List<ApiListResponse> selectedFiles = [];
 
   final ReceivePort _port = ReceivePort();
+
+  _setSelectMode(bool val) {
+    if (val) {
+      selectMode = true;
+    } else {
+      selectMode = false;
+      selectedFiles = [];
+    }
+    setState(() {});
+  }
 
   @override
   void initState() {
     super.initState();
+
+    //* Get data on init
+    _fetchData().then((data) {
+      setState(() {
+        _data = data; //* Store the initial data in the separate state variable
+      });
+    });
+
+    BackButtonInterceptor.add(exitSelectModeBack);
 
     IsolateNameServer.registerPortWithName(_port.sendPort, 'downloader_send_port');
     /* _port.listen((dynamic data) {
@@ -42,8 +66,18 @@ class _MainPageState extends State<MainPage> {
 
   @override
   void dispose() {
+    BackButtonInterceptor.remove(exitSelectModeBack);
     IsolateNameServer.removePortNameMapping('downloader_send_port');
     super.dispose();
+  }
+
+  bool exitSelectModeBack(bool stopDefaultButtonEvent, RouteInfo info) {
+    if (selectMode) {
+      _setSelectMode(false);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   void _download(String url) async {
@@ -58,37 +92,56 @@ class _MainPageState extends State<MainPage> {
     );
   }
 
-  Future<ApiListResponseList?> _fetchData() async {
-    final pathDir = widget.currentDir == null ? '/' : widget.currentDir!.path;
-    final response = await http.get(Uri.parse('$apiUrl/list$pathDir'));
-    if (response.statusCode == 200) {
-      var parsedResponse = ApiListResponseList.fromJson(jsonDecode(response.body));
-      parsedResponse.files.sort((a, b) {
-        if (a.isDirectory && b.isDirectory) return a.name.compareTo(b.name);
-        if (a.isDirectory && !b.isDirectory) return -1;
-        if (!a.isDirectory && b.isDirectory) return 1;
-        return a.name.compareTo(b.name);
-      });
-      return parsedResponse;
+  _loadAppBar() {
+    if (selectMode) {
+      final selectedFilesCount = selectedFiles.length;
+      return AppBar(
+        backgroundColor: const Color.fromARGB(255, 71, 71, 71),
+        leading: IconButton(
+            splashRadius: 24,
+            onPressed: () {
+              _setSelectMode(false);
+            },
+            icon: const Icon(Icons.close)),
+        title: Text('$selectedFilesCount File(s) selected'),
+      );
     } else {
-      return null;
+      return AppBar(
+        leading: widget.currentDir == null
+            ? null
+            : IconButton(
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+                icon: const Icon(Icons.arrow_back)),
+        title: Text(widget.currentDir?.name ?? 'File Server'),
+      );
     }
   }
 
-  _loadUI(AsyncSnapshot<ApiListResponseList?> snapshot) {
-    if (snapshot.connectionState == ConnectionState.done) {
-      if (snapshot.data != null) {
+  _loadUI() {
+    if (connectionDone) {
+      if (_data != null) {
         return ListView.builder(
-            itemCount: snapshot.data!.files.length,
+            itemCount: _data!.files.length,
             itemBuilder: (context, index) {
               return ListTile(
-                leading: Icon(_getIcon(snapshot.data!.files[index])),
-                title: Text(snapshot.data!.files[index].name),
+                tileColor: selectedFiles.contains(_data!.files[index]) ? Colors.grey : Colors.transparent,
+                leading: selectMode && selectedFiles.contains(_data!.files[index])
+                    ? const Icon(Icons.done)
+                    : Icon(_getIcon(_data!.files[index])),
+                title: Text(_data!.files[index].name),
                 onTap: () async {
-                  if (snapshot.data!.files[index].isDirectory) {
+                  if (selectMode) {
+                    selectedFiles.contains(_data!.files[index])
+                        ? selectedFiles.remove(_data!.files[index])
+                        : selectedFiles.add(_data!.files[index]);
+                    return setState(() {});
+                  }
+                  if (_data!.files[index].isDirectory) {
                     Navigator.push(
                       context,
-                      MaterialPageRoute(builder: (context) => MainPage(currentDir: snapshot.data!.files[index])),
+                      MaterialPageRoute(builder: (context) => MainPage(currentDir: _data!.files[index])),
                     );
                   } /* else if (_getIcon(snapshot.data!.files[index]) == Icons.image) { //TODO: Reenable these after improving them
                     final imagePath = snapshot.data!.files[index].path;
@@ -108,9 +161,13 @@ class _MainPageState extends State<MainPage> {
                     );
                   } */
                   else {
-                    final filePath = snapshot.data!.files[index].path;
+                    final filePath = _data!.files[index].path;
                     _download('$apiUrl/retrieve$filePath');
                   }
+                },
+                onLongPress: () {
+                  selectedFiles.add(_data!.files[index]);
+                  _setSelectMode(true);
                 },
               );
             });
@@ -123,6 +180,37 @@ class _MainPageState extends State<MainPage> {
           color: Theme.of(context).colorScheme.secondary,
         ),
       );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: _loadAppBar(), body: _loadUI());
+  }
+
+  Future<void> _refreshData() async {
+    final newData = await _fetchData(); // fetch new data from the API
+    setState(() {
+      _data = newData; // update the separate state variable with the new data
+    });
+  }
+
+  Future<ApiListResponseList?> _fetchData() async {
+    final pathDir = widget.currentDir == null ? '/' : widget.currentDir!.path;
+    final response = await http.get(Uri.parse('$apiUrl/list$pathDir'));
+    if (response.statusCode == 200) {
+      var parsedResponse = ApiListResponseList.fromJson(jsonDecode(response.body));
+      parsedResponse.files.sort((a, b) {
+        if (a.isDirectory && b.isDirectory) return a.name.compareTo(b.name);
+        if (a.isDirectory && !b.isDirectory) return -1;
+        if (!a.isDirectory && b.isDirectory) return 1;
+        return a.name.compareTo(b.name);
+      });
+      connectionDone = true;
+      return parsedResponse;
+    } else {
+      connectionDone = true;
+      return null;
     }
   }
 
@@ -142,25 +230,5 @@ class _MainPageState extends State<MainPage> {
     if (['ass', 'srt', 'vtt'].contains(extension)) return Icons.closed_caption;
     if (['exe'].contains(extension)) return Icons.terminal;
     return Icons.insert_drive_file;
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return FutureBuilder(
-        future: _fetchData(),
-        builder: (BuildContext context, AsyncSnapshot<ApiListResponseList?> snapshot) {
-          return Scaffold(
-              appBar: AppBar(
-                leading: widget.currentDir == null
-                    ? null
-                    : IconButton(
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
-                        icon: const Icon(Icons.arrow_back)),
-                title: Text(widget.currentDir?.name ?? 'File Server'),
-              ),
-              body: _loadUI(snapshot));
-        });
   }
 }
